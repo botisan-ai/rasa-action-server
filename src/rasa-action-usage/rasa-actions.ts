d00fcb770639dfdded17d6d75ed2f548b26a024d
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2021-09-04 12:49:21 +0300

    test DI

--- src/rasa-action-usage/rasa-actions.ts
+++ src/rasa-action-usage/rasa-actions.ts
@@ -1,10 +1,15 @@
+import { ModuleRef } from '@nestjs/core';
 import { IActionDispatcher, IActionDomain, IActionTracker, IEvent, IRunnableAction } from '../../sdk';
 import { Action } from '../rasa-action/rasa-action.decorators';
 
 @Action({ name: 'hello_action' })
 export class HelloAction implements IRunnableAction {
+  // Inject module ref to prove Nest Dependency Injection works.
+  constructor(private readonly ref: ModuleRef) {}
+
   async run(tracker: IActionTracker, dispatcher: IActionDispatcher, domain: IActionDomain): Promise<IEvent<any>[] | void> {
     console.log(tracker, dispatcher, domain);
+    console.log(`DI works! : ${!!this.ref}\n`);
     return;
   }
 }
